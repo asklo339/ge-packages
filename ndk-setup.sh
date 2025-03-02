40f0c06eebc9aaf75596c36ec8cc42a1ff9e66fa
asklo339 <davitysk@gmail.com>
2025-03-02 18:30:24 +0530

    Update ndk-setup.sh

--- ndk-setup.sh
+++ ndk-setup.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 set -e
 
-# Download and install NDK r26b
+# ndk-setup.sh
+# Downloads and sets up the Android NDK.
 NDK_VERSION="r26b"
-NDK_URL="https://dl.google.com/android/repository/android-ndk-${NDK_VERSION}-linux.zip"
-wget "$NDK_URL" -O /tmp/android-ndk.zip
-unzip /tmp/android-ndk.zip -d /opt
-rm /tmp/android-ndk.zip
+NDK_ZIP="android-ndk-${NDK_VERSION}-linux.zip"
+NDK_DIR="/opt/android-ndk-${NDK_VERSION}"
 
-# Set environment variables
-export ANDROID_NDK_HOME="/opt/android-ndk-${NDK_VERSION}"
-export PATH="$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH"
+if [ ! -d "${NDK_DIR}" ]; then
+    echo "Downloading Android NDK ${NDK_VERSION}..."
+    wget https://dl.google.com/android/repository/${NDK_ZIP} -O /tmp/${NDK_ZIP}
+    unzip /tmp/${NDK_ZIP} -d /opt
+    rm /tmp/${NDK_ZIP}
+fi
+
+export ANDROID_NDK="${NDK_DIR}"
+echo "ANDROID_NDK is set to ${ANDROID_NDK}"
